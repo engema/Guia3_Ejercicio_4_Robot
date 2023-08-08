12f5359898bffc3e825f10e2fb161fbb6d93602c
Quique <44074048+engema@users.noreply.github.com>
2023-08-08 17:07:21 -0300

    Cambio de Quique
    Se agregó garabato a comentario

--- src/guia3_ejercicio_4_robot/Guia3_Ejercicio_4_Robot.java
+++ src/guia3_ejercicio_4_robot/Guia3_Ejercicio_4_Robot.java
@@ -17,7 +17,7 @@ public class Guia3_Ejercicio_4_Robot {
         Hombre hombre1=new Hombre(robot1);
         Hombre hombre2=new Hombre(robot1);
         
-       /* hombre1.jugarConRobot(robot1);
+       /* hombre1.jugarConRobot(robot1);ljasdhljasdhljasd
        hombre2.getRobot1().despertar();
         hombre2.jugarConRobot(robot1);*/
         
